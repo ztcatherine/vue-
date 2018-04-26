e92d29fb6fd82400c3e2e3831539641fa37bb4fb
周童 <904654835@qq.com>
2018-04-26 09:52:40 +0800

    vue

---
+++ src/components/app.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="continer">
+    <div id="header">请发表对Vue的评论</div>
+    <div id="content">
+      <add :addComment="addComment"></add>
+      <list :comments="comments" :deleteComment="deleteComment"></list>
+    </div>
+  </div>
+</template>
+
+<script>
+  import add from './add.vue'
+  import list from './list.vue'
+  export default {
+    data(){
+      return{
+        comments:[
+          {name:'Tom',content:'Vue还不错！'},
+          {name:'Jack',content:'Vue挺好！'}
+        ]
+      }
+    },
+
+    methods:{
+      addComment(comment){
+        this.comments.unshift(comment)
+      },
+      deleteComment(index){
+        this.comments.splice(index,1)
+      }
+    },
+    components:{
+      add,list
+    }
+  }
+</script>
+
+<style>
+  *{
+    margin: 0;
+    padding: 0;
+  }
+  html,body{
+    width: 100%;
+    height: 1000px;
+  }
+  #continer{
+    width: 100%;
+    height: 100%;
+  }
+  #header{
+    height: 150px;
+    background-color: #f0f0f0;
+    display: flex;
+    align-items: center;
+    padding-left: 50px;
+    font-size: 20px;
+  }
+  #content{
+    width: 100%;
+    display: flex;
+  }
+
+</style>
---
+++ src/components/item.vue
@@ -0,0 +1,47 @@
+<template>
+  <li class="list-group">
+    <div class="del">
+      <a href="#" @click="remove">删除</a>
+    </div>
+    <p class="user"><span>{{comment.name}}</span><span>说：</span></p>
+    <p class="centens">{{comment.content}}</p>
+  </li>
+</template>
+
+<script>
+  export default {
+    props:['comment','deleteComment','index'],
+
+    methods:{
+      remove(){
+        const {comment,deleteComment,index} = this
+        if(confirm(`确定删除${comment.name}的评论吗？`)){
+          deleteComment(index)
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+  #group .list-group{
+    list-style: none;
+    border:1px solid gainsboro;
+    padding: 10px;
+  }
+  .del{
+    width: 34px;
+    float: right;
+    border: 1px solid;
+  }
+  .del a{
+    text-decoration: none;
+  }
+  .user{
+    font-size: 18px;
+  }
+  .centens{
+    font-size: 14px;
+    margin: 10px 0 10px 30px ;
+  }
+</style>
